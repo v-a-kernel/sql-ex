b8ab382e985a7ddee8d8f699ac311f02d1de5519
Vadim A. Kazantsev <v.a.kernel@gmail.com>
2017-08-24 21:44:58 +0300

    Added new file: exercises_from_sql-ex.ru/sql-ex-025.sql with solution for/to 25th exercise:
    -- Найдите производителей принтеров, которые производят ПК с наименьшим
    -- объёмом RAM и с самым быстрым процессором среди всех ПК, имеющих
    -- наименьший объём RAM.
    -- Вывести: Maker.
    
    SELECT DISTINCT maker
        FROM product
        WHERE model IN (SELECT model
                            FROM pc
                            WHERE ram = (SELECT MIN(ram) FROM pc)
                                AND speed = (SELECT MAX(speed)
                                                 FROM pc
                                                 WHERE ram = (SELECT MIN(ram)
                                                                  FROM pc
                                                              )
                                             )
                        )
            AND maker IN (SELECT maker
                              FROM product
                              WHERE type = 'printer');

---
+++ exercises_from_sql-ex.ru/sql-ex-025.sql
@@ -0,0 +1,20 @@
+-- Найдите производителей принтеров, которые производят ПК с наименьшим 
+-- объёмом RAM и с самым быстрым процессором среди всех ПК, имеющих 
+-- наименьший объём RAM.  
+-- Вывести: Maker.
+
+SELECT DISTINCT maker 
+    FROM product 
+    WHERE model IN (SELECT model 
+                        FROM pc 
+                        WHERE ram = (SELECT MIN(ram) FROM pc) 
+                            AND speed = (SELECT MAX(speed) 
+                                             FROM pc 
+                                             WHERE ram = (SELECT MIN(ram) 
+                                                              FROM pc 
+                                                          ) 
+                                         ) 
+                    ) 
+        AND maker IN (SELECT maker 
+                          FROM product 
+                          WHERE type = 'printer');
